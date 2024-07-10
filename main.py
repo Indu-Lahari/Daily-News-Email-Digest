51e70582ac2decafc68e05a493a8d471b4a2fcda
Indu <indulahari6@gmail.com>
2024-07-10 15:54:55 +0530

    Access the article titles and descriptions from API

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ api_key = "b79705e5c12f417cbcff102e5ee27af2"
 
 url = ("https://newsapi.org/v2/everything?q=apple&from=2024-07-09&to=2024-07-09&sortBy=popularity&apiKey=b79705e5c12f417cbcff102e5ee27af2")
 
+# Make requests
 r = requests.get(url)
-content = r.text
-print(content)
+
+# r.text is changed to r.json() to get data as dict not as str
+content = r.json()
+
+# Access the article titles and descriptions
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
