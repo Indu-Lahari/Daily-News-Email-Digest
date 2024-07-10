dafdf251db47430dc248dfe4a7504042e624526f
Indu <indulahari6@gmail.com>
2024-07-10 23:39:16 +0530

    Using List slicing to send only 20 articles/news

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from send_mail import news_digest
 
 api_key = "b79705e5c12f417cbcff102e5ee27af2"
 
-url = ("https://newsapi.org/v2/everything?q=apple&from=2024-07-09&to=2024-07-09&sortBy=popularity&apiKey=b79705e5c12f417cbcff102e5ee27af2&language=en")
+url = "https://newsapi.org/v2/everything?q=apple&from=2024-07-09&to=2024-07-09&sortBy=popularity&apiKey=b79705e5c12f417cbcff102e5ee27af2&language=en"
 
 # Make requests
 r = requests.get(url)
@@ -13,9 +13,11 @@ content = r.json()
 
 news = ""
 # Access the article titles and descriptions
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        news = news + article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
+        news = (news + article["title"] + "\n"
+                + article["description"] + "\n"
+                + article["url"] + 2*"\n")
 
 news = news.encode("UTF-8")
 news_digest(news)
